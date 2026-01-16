64d930381766418bdaa04b19b98fde3284e68f84
LoistCloist <heinzarniaung22@gmail.com>
2026-01-16 14:36:58 -0500

    Overhauled login/register page.

--- todolist-app/app/login/page.tsx
+++ todolist-app/app/login/page.tsx
@@ -3,36 +3,99 @@ import {
   Anchor,
   Button,
   Checkbox,
+  Divider,
+  Group,
   Paper,
+  PaperProps,
   PasswordInput,
+  Stack,
   Text,
   TextInput,
-  Title,
+  Box,
+  Center
 } from '@mantine/core';
-import classes from './AuthenticationImage.module.css';
+import { useForm } from '@mantine/form';
+import { upperFirst, useToggle } from '@mantine/hooks';
 
-export default function AuthenticationImage() {
+export default function AuthenticationForm(props: PaperProps) {
+  const [type, toggle] = useToggle(['login', 'register']);
+  const form = useForm({
+    initialValues: {
+      email: '',
+      name: '',
+      password: '',
+      terms: true,
+    },
+
+    validate: {
+      email: (val) => (/^\S+@\S+$/.test(val) ? null : 'Invalid email'),
+      password: (val) => (val.length <= 6 ? 'Password should include at least 6 characters' : null),
+    },
+  });
+
   return (
-    <div className={classes.wrapper}>
-      <Paper className={classes.form}>
-        <Title order={2} className={classes.title}>
-          Welcome back to Mantine!
-        </Title>
+    <Center h="100vh">
+    <Paper radius="md" p="lg" withBorder {...props}>
+      <Text size="lg" fw={500}>
+        Welcome to Mantine, {type} with
+      </Text>
 
-        <TextInput label="Email address" placeholder="hello@gmail.com" size="md" radius="md" />
-        <PasswordInput label="Password" placeholder="Your password" mt="md" size="md" radius="md" />
-        <Checkbox label="Keep me logged in" mt="xl" size="md" />
-        <Button fullWidth mt="xl" size="md" radius="md">
-          Login
-        </Button>
 
-        <Text ta="center" mt="md">
-          Don&apos;t have an account?{' '}
-          <Anchor href="#" fw={500} onClick={(event) => event.preventDefault()}>
-            Register
+      <Divider label="Or continue with email" labelPosition="center" my="lg" />
+
+      <form onSubmit={form.onSubmit(() => {})}>
+        <Stack>
+          {type === 'register' && (
+            <TextInput
+              label="Name"
+              placeholder="Your name"
+              value={form.values.name}
+              onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
+              radius="md"
+            />
+          )}
+
+          <TextInput
+            required
+            label="Email"
+            placeholder="hello@mantine.dev"
+            value={form.values.email}
+            onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
+            error={form.errors.email && 'Invalid email'}
+            radius="md"
+          />
+
+          <PasswordInput
+            required
+            label="Password"
+            placeholder="Your password"
+            value={form.values.password}
+            onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
+            error={form.errors.password && 'Password should include at least 6 characters'}
+            radius="md"
+          />
+
+          {type === 'register' && (
+            <Checkbox
+              label="I accept terms and conditions"
+              checked={form.values.terms}
+              onChange={(event) => form.setFieldValue('terms', event.currentTarget.checked)}
+            />
+          )}
+        </Stack>
+
+        <Group justify="space-between" mt="xl">
+          <Anchor component="button" type="button" c="dimmed" onClick={() => toggle()} size="xs">
+            {type === 'register'
+              ? 'Already have an account? Login'
+              : "Don't have an account? Register"}
           </Anchor>
-        </Text>
-      </Paper>
-    </div>
+          <Button type="submit" radius="xl">
+            {upperFirst(type)}
+          </Button>
+        </Group>
+      </form>
+    </Paper>
+    </Center>
   );
 }
